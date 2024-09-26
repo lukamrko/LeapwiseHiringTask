413b8f8626f02c1c951690c1849b71c65fb69791
lukamrko <lukamrko@gmail.com>
2024-09-26 11:48:49 +0200

    Inital project creation

---
+++ src/main/java/lmrkonjic/leapwisehiringtask/LeapwiseHiringTaskApplication.java
@@ -0,0 +1,13 @@
+package lmrkonjic.leapwisehiringtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapwiseHiringTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeapwiseHiringTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lmrkonjic/leapwisehiringtask/LeapwiseHiringTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package lmrkonjic.leapwisehiringtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeapwiseHiringTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
